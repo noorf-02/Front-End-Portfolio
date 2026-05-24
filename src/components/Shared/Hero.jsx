7a5b3e3a059ab9041d1b0a13b74b9da5f365f8a8
Noor Fatima <noor2239064@gmail.com>
2026-05-25 00:47:06 +0500

    handled decrement

--- src/components/Shared/Hero.jsx
+++ src/components/Shared/Hero.jsx
@@ -4,6 +4,11 @@ import { useState } from 'react'
 export default function Hero() {
 
     const [ value, setValue ] = useState(0)
+    const handleDec = () => {
+        if(value>0){
+            setValue(value -1)
+        }
+    }
 
   return (
     <>
@@ -27,7 +32,7 @@ export default function Hero() {
     <h1 className='counter-heading text-2xl font-bold'> Counter - UseState Example </h1>
     <span className='bg-gray-500 p-1 px-2 rounded-[5px] w-[100px] text-center cursor-pointer'>{value}</span>
     <div className='counter-buttons flex gap-3'>
-    <button className='bg-green-500 p-1 px-2 rounded-[5px] w-[100px] cursor-pointer hover:bg-green-600' onClick={()=> setValue(value - 1 )}>Decrement</button>
+    <button className='bg-green-500 p-1 px-2 rounded-[5px] w-[100px] cursor-pointer hover:bg-green-600' onClick={handleDec}>Decrement</button>
     <button className='bg-yellow-500 p-1 px-2 rounded-[5px] w-[100px] cursor-pointer hover:bg-yellow-600' onClick={()=> setValue(value + 1)}>Increment</button>
     <button className='bg-red-500 p-1 px-2 rounded-[5px] w-[100px] cursor-pointer hover:bg-red-600' onClick={()=> setValue(0)}>Reset</button>
     </div>
