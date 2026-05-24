5853649f6826b2d00483e4c8ae338bd050d64e33
Noor Fatima <noor2239064@gmail.com>
2026-05-24 22:13:33 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LandingPage from './Pages/LandingPage'
+
+export default function App() {
+  return (
+    <div>
+      <LandingPage/>
+    </div>
+  )
+}
---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Hero from '../components/Shared/Hero'
+
+export default function LandingPage() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  )
+}
