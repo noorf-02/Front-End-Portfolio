cf4f0ea18d2640327516793b939ddd970fc8c61d
Noor Fatima <noor2239064@gmail.com>
2026-05-25 21:13:55 +0500

    sidebar working

--- src/components/Shared/Hero.jsx
+++ src/components/Shared/Hero.jsx
@@ -1,84 +1,82 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
 export default function Hero() {
+  // const [ value, setValue ] = useState(0)
+  // const handleDec = () => {
+  //     if(value>0){
+  //         setValue(value -1)
+  //     }
+  // }
 
-    const [ value, setValue ] = useState(0)
-    const handleDec = () => {
-        if(value>0){
-            setValue(value -1)
-        }
-    }
-
-    const { isOpen , setOpen } = useState(false)
-
+  const [isOpen , setOpen ] = useState(false);
 
   return (
     <>
-    <div className='w-[90%] mx-auto text-white py-3.5'>
-      <nav className='flex justify-between items-center'>
-        <span className="border-[#222222] border-[2px] text-[15px] p-3 rounded-[50%] bg-[#1A1A1A]/70 font-bold text-white cursor-pointer hover:bg-[#2A2A2A]/70">nf</span>
-        <div className="nav-list gap-7.5 border-[#222222] border-[2px] py-3.5 px-8.5 rounded-2xl bg-[#1A1A1A]/70 font-bold text-white cursor-pointer sm:flex hidden ">
-            <div className="nav-items relative after:absolute after:-bottom-1 after:w-0 after:bg-white/70 hover:after:w-full after:left-0 after:h-0.5 after:transition-all after:duration-300 ">Home</div>
-            <div className="nav-items relative after:absolute after:-bottom-1 after:w-0 after:bg-white/70 hover:after:w-full after:left-0 after:h-0.5 after:transition-all after:duration-300 ">Projects</div>
-            <div className="nav-items relative after:absolute after:-bottom-1 after:w-0 after:bg-white/70 hover:after:w-full after:left-0 after:h-0.5 after:transition-all after:duration-300 ">Services</div>
-            <div className="nav-items relative after:absolute after:-bottom-1 after:w-0 after:bg-white/70 hover:after:w-full after:left-0 after:h-0.5 after:transition-all after:duration-300 ">Contact</div>
-        </div>
-        <div className='flex gap-5 items-center'><button className="talk border-[#222222] border-[2px] p-3 rounded-2xl bg-[#1A1A1A]/70 font-bold text-white cursor-pointer hover:bg-[#2a2a2a]/70">Let's Talk!</button>
+      <div className="w-[90%] mx-auto text-white py-3.5">
+        <nav className="flex justify-between items-center">
+          <span className="border-[#222222] border-[2px] text-[15px] p-3 rounded-[50%] bg-[#1A1A1A]/70 font-bold text-white cursor-pointer hover:bg-[#2A2A2A]/70">
+            nf
+          </span>
+          <div className="nav-list gap-7.5 border-[#222222] border-[2px] py-3.5 px-8.5 rounded-2xl bg-[#1A1A1A]/70 font-bold text-white cursor-pointer sm:flex hidden ">
+            <div className="nav-items relative after:absolute after:-bottom-1 after:w-0 after:bg-white/70 hover:after:w-full after:left-0 after:h-0.5 after:transition-all after:duration-300 ">
+              Home
+            </div>
+            <div className="nav-items relative after:absolute after:-bottom-1 after:w-0 after:bg-white/70 hover:after:w-full after:left-0 after:h-0.5 after:transition-all after:duration-300 ">
+              Projects
+            </div>
+            <div className="nav-items relative after:absolute after:-bottom-1 after:w-0 after:bg-white/70 hover:after:w-full after:left-0 after:h-0.5 after:transition-all after:duration-300 ">
+              Services
+            </div>
+            <div className="nav-items relative after:absolute after:-bottom-1 after:w-0 after:bg-white/70 hover:after:w-full after:left-0 after:h-0.5 after:transition-all after:duration-300 ">
+              Contact
+            </div>
+          </div>
+          <div className="flex gap-5 items-center">
+            <button className="talk border-[#222222] border-[2px] p-3 rounded-2xl bg-[#1A1A1A]/70 font-bold text-white cursor-pointer hover:bg-[#2a2a2a]/70">
+              Let's Talk!
+            </button>
 
-        
-        
-        
-        <span className=' block sm:hidden' ><i className="fa-solid fa-bars text-white">
-          
-          <div className="side-bar h-screen fixed right-0 top-0 bg-[#1A1A1A] w-60 p-6">
-            <i class="fa-solid fa-xmark cursor-pointer fixed right-7"></i>
-            
-             <div className="flex
+            <span className=" block sm:hidden">
+              <i
+                className="fa-solid fa-bars text-white "
+                onClick={() => setOpen(!isOpen)}
+              ></i>
+            </span>
+            <div className={`side-bar h-screen fixed right-0 top-0 bg-[#1A1A1A] w-60 p-6 block sm:hidden ${isOpen ? 'block' : 'hidden '}`}>
+              <i class="fa-solid fa-xmark cursor-pointer fixed right-7" 
+              onClick={()=>setOpen(false)}></i>
+
+              <div
+                className="flex
              flex-col
              text-left
              h-50
              justify-between 
              mt-10
-             font-medium">
-            <div className="cursor-pointer">Home</div>
-            <hr />
-            <div className="cursor-pointer ">Projects</div>
-            <hr />
-            <div className="cursor-pointer">Services</div>
-            <hr />
-            <div className="cursor-pointer">Contact</div>
-            <hr />
-            <div className="sm-icons">
-              <i class="fa-brands fa-linkedin cursor-pointer"></i>
-              <i class="fa-brands fa-github cursor-pointer"></i>
+             font-medium"
+              >
+                <div className="cursor-pointer">Home</div>
+                <hr />
+                <div className="cursor-pointer ">Projects</div>
+                <hr />
+                <div className="cursor-pointer">Services</div>
+                <hr />
+                <div className="cursor-pointer">Contact</div>
+                <hr />
+                <div className="sm-icons">
+                  <i class="fa-brands fa-linkedin cursor-pointer"></i>
+                  <i class="fa-brands fa-github cursor-pointer"></i>
+                </div>
+              </div>
             </div>
-        </div>
+          </div>
+        </nav>
+      </div>
 
-            
-            </div>
-          
-          
-          
-          
-          </i></span>
-        </div>
-      </nav>
-    </div>
+      {/* counter  */}
 
-
-
-
-
-
-
-
-
-
-
-    {/* counter  */}
-
-{/* <div className='counter text-white w-[90%] mx-auto flex flex-col items-center py-9.5 gap-5'>
+      {/* <div className='counter text-white w-[90%] mx-auto flex flex-col items-center py-9.5 gap-5'>
     <h1 className='counter-heading text-2xl font-bold'> Counter - UseState Example </h1>
     <span className='bg-gray-500 p-1 px-2 rounded-[5px] w-[100px] text-center cursor-pointer'>{value}</span>
     <div className='counter-buttons flex gap-3'>
@@ -87,10 +85,6 @@ export default function Hero() {
     <button className='bg-red-500 p-1 px-2 rounded-[5px] w-[100px] cursor-pointer hover:bg-red-600' onClick={()=> setValue(0)}>Reset</button>
     </div>
 </div> */}
-
-
-
     </>
-    
-  )
+  );
 }
