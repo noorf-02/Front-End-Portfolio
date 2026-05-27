fa86ce46ef1f8d39a3a54af9d8cce9569989eb23
Noor Fatima <noor2239064@gmail.com>
2026-05-27 23:37:45 +0500

    added framer motion to hero section

--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { motion } from "framer-motion";
 
 export default function Hero() {
   return (
     <div className="wrapper text-white">
-      <section className="hero h-[70vh] flex flex-col justify-center">
+      <motion.div initial={{opacity:0,x:-50}} whileInView={{opacity:1,x:0}} viewport={{once:true}} transition={{duration:0.4}} className="hero h-[70vh] flex flex-col justify-center">
           <p className="sm:text-[18px] text-15px font-bold">I am Noor</p>
           <h1 className="sm:text-4xl text-2xl font-bold bg-gradient-to-r from-[#4fd6c6] to-[#4e379e] bg-clip-text text-transparent">Front-End Developer & SEO specialist</h1>
           <p className="pt-3 flex-wrap sm:flex-nowrap text-[13px] sm:text-[20px]">Frontend Developer & SEO Specialist skilled in creating responsive, user-friendly websites and optimizing them for better performance and search engine visibility.</p>
@@ -15,7 +16,7 @@ export default function Hero() {
                   <a href="https://www.linkedin.com/" target="_blank"><i class="fa-brands fa-linkedin cursor-pointer text-[18px] sm:text-[25px] "></i></a>
                   <a href="https://github.com/noorf-02" target="_blank"><i class="fa-brands fa-github cursor-pointer text-[18px] sm:text-[25px] "></i></a>
                 </div>
-        </section>
+        </motion.div>
     </div>
   )
 }
