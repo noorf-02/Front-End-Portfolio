41e5cf05b52c382923a46e763d1c625be014a086
Noor Fatima <noor2239064@gmail.com>
2026-06-01 22:45:08 +0500

    added footer

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <div className='text-white'>
-      Footer
+    <div className="footer bg-gradient-to-r from-[#4fd6c6] to-[#4e379e] w-full h-15 items-center flex">
+      <div className="wrapper flex justify-end sm:gap-3 gap-1">
+        <button className="border-[1px] bg-black text-white border-black rounded-[13px] p-2 w-20 hover:bg-[#1e1e1e] cursor-pointer transition-all duration-200 sm:text-[15px] text-[13px]">
+          LinkedIn
+        </button>
+        <button className="border-[1px] bg-black text-white border-black rounded-[13px] p-2 w-20 hover:bg-[#1e1e1e] cursor-pointer transition-all duration-200 sm:text-[15px] text-[13px]">
+          GitHub
+        </button>
+      </div>
     </div>
-  )
+  );
 }
