d28788bb5c9328efa6e6c2beb554d39357729002
Noor Fatima <noor2239064@gmail.com>
2026-06-02 00:48:45 +0500

    gave color animation

--- src/components/UI/SectionContact.jsx
+++ src/components/UI/SectionContact.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function SectionContact() {
   return (
-    <div>
-      contact
+    <div className='contact'>
+      <div className='wrapper flex gap-2'>
+        <a href="https://github.com/noorf-02" target='_blank'><button className='animate-[color_5s_ease-in-out_infinite] border-1 bg-white rounded-2xl p-1 px-3 w-fit'>GitHub<i className="fa-brands fa-github"></i></button></a>
+        <a href="https://www.linkedin.com/in/noor-fatima-imran/" target='_blank'><button className='animate-[color_5s_ease-in-out_infinite] border-1 bg-white rounded-2xl p-1 px-3 w-fit'>LinkedIn<i className="fa-brands fa-linkedin-in"></i></button></a>
+      </div>
     </div>
   )
 }
